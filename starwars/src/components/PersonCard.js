5be4e59bd56eb9fac6b44ed045f0cdbe1f9dbccd
Amber Mohler <ambermohler@imac.lan>
2019-12-13 11:42:01 -0500

    created PersonCard component with dummy text in to get it rendering on the app, success so far

---
+++ starwars/src/components/PersonCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const PersonCard = props => {
+return (
+    <div>
+        <p>Name</p>
+        <p>Height</p>
+        <p>Weight</p>
+        <p>Hair Color</p>
+        <p>Skin Color</p>
+        <p>Eye Color</p>
+        <p>birth year</p>
+    </div>
+);
+};
+
+export default PersonCard;
--- starwars/src/components/PersonGetter.js
+++ starwars/src/components/PersonGetter.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import PersonCard from './PersonCard';
 
 
 
 export default function PersonGetter() {
-    const [person, setPerson] = useState()
+    const [person, setPerson] = useState([])
 
 
 useEffect(() => {
@@ -19,7 +20,9 @@ useEffect(() => {
 }, [])
 
 return (
-    <div></div>
+    <div>
+        <PersonCard />
+    </div>
 )
 
 }
